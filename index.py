2b1ce6a312e3adbddfd904357d13ff3823864c4b
Fred <engineerfretchy99@gmail.com>
2024-07-03 11:47:00 +0100

    my python beginning

---
+++ index.py
@@ -0,0 +1,42 @@
+# x=5
+# # y=6
+
+# # print(x+y)
+
+
+
+# print(""" My name is Fred
+#      gooner
+#      learning data analysis
+# """)
+
+# print('''
+#     ghedhu
+#       uyeyd
+#       uiedqy
+
+#   ''')
+
+# names='Fred','Bimbo','Deji'i
+# print(names)
+
+# apple = potato =carrot = "#2000"
+# print(apple)
+
+x,y,z=2, 4, 5
+print(x)
+print(y)
+print(z)
+
+# concatenation
+
+firstName = 'Fred'
+lastName = 'Uche'
+occupation = 'Data analyst'
+id = 8
+
+
+
+# print('my name is '+firstName+' '+lastName+' i am a '+occupation+'. my id is '+str(id))
+
+print(f"my name is {firstName} {lastName} i am a {occupation}. my id is {id} ")
