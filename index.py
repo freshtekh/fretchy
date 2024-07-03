45e161f691596d765f95095d9db154a0a80a8a37
Fred <engineerfretchy99@gmail.com>
2024-07-03 12:51:19 +0100

    py2.

--- index.py
+++ index.py
@@ -23,28 +23,42 @@
 # apple = potato =carrot = "#2000"
 # print(apple)
 
-x,y,z=2, 4, 5
-print(x)
-print(y)
-print(z)
+# x,y,z=2, 4, 5
+# print(x)
+# print(y)
+# print(z)
 
 # concatenation
 
-firstName = 'Fred'
-lastName = 'Uche'
-occupation = 'Data analyst'
-id = 8
+# firstName = 'Fred'
+# lastName = 'Uche'
+# occupation = 'Data analyst'
+# id = 8
 
 
 
 # print('my name is '+firstName+' '+lastName+' i am a '+occupation+'. my id is '+str(id))
 
-print(f"my name is {firstName} {lastName} i am a {occupation}. my id is {id} ")
+# print(f"my name is {firstName} {lastName} i am a {occupation}. my id is {id} ")
 
-# number types
+# # number types
 
-number= 23+2j
-print(type(number))
+# number= 23+2j
+# print(type(number))
 
-var=True
-print(type(var))
+# var=True
+# print(type(var))
+
+# items = ['Pepper','Tomato','Fish']
+# print(type(items))
+
+# items = ('Pepper','Tomato','Fish')
+# print(type(items))
+
+# var= list(range(1,11,2))
+# print(var)
+
+# var={'Pepper','Tomato','Fish'}
+
+student1= {'Id':1, "Name":"Fred", "Occupation":"Data analyst"}
+# print()
